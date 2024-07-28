d9a677c25323e7cfccaed2d5599cbabe741b7b7c
Narai Risser (RIT Student) <ngr7989@rit.edu>
2024-07-27 21:05:51 -0400

    key room

--- Scripts/CollectionCircle.gd
+++ Scripts/CollectionCircle.gd
@@ -1,11 +1,39 @@
-extends Sprite2D
+extends Area2D
+@export var sprite : Sprite2D
+@export var wobCurve : Curve
+
+var wobbleLimits = Vector2(-.3, .3)
+var wobbleSpeed = 1
+var wobbleLerp = 0.5
+var wobbleIncrease : bool
+var canWobble : bool
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+func _init():
+	canWobble = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	Wobble(delta)
+
+func Wobble(delta):
+	if canWobble == false:
+		return
+	
+	sprite.rotation = lerp(wobbleLimits.x, wobbleLimits.y, wobCurve.sample(wobbleLerp))
+	if wobbleIncrease:
+		wobbleLerp += delta * wobbleSpeed
+		if wobbleLerp >= 1:
+			wobbleLerp = 1
+			wobbleIncrease = false
+	else:
+		wobbleLerp -= delta * wobbleSpeed
+		if wobbleLerp <= 0:
+			wobbleLerp = 0
+			wobbleIncrease = true
+
+func StartWobble(area : Area2D):
+	canWobble = true
+
+func StopWobble(area : Area2D):
+	canWobble = false
