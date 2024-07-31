6c8ca31a16f1e4ab40d2930f1985b777beb79fb8
Narai Risser (RIT Student) <ngr7989@rit.edu>
2024-07-30 23:32:53 -0400

    Pause in beginning

--- Scripts/Transitions.gd
+++ Scripts/Transitions.gd
@@ -11,6 +11,8 @@ extends Node2D
 @export var closedTop : Vector2
 @export var closedBot : Vector2
 
+@export var pauseBeforeOpen : float # Pause at the very beginning 
+
 var topStart : Vector2
 var botStart : Vector2
 
@@ -20,17 +22,32 @@ var doneAnim : bool
 var state : transStates
 enum transStates {IDLE, TRANS_IN, TRANS_OUT}
 
+
+var hasBegun : bool 
+var begunTimer : float 
+
 func _init():
 	doneAnim = true
+	hasBegun = false
+	begunTimer = 0
 	state = transStates.IDLE
 
 func _ready():
 	topStart = topSaw.position
 	botStart = botSaw.position
+	SetSawPos(1)
 	
-	TransitionIn()
 
 func _process(delta):
+	if !hasBegun:
+		begunTimer += delta
+		
+		if begunTimer >= pauseBeforeOpen:
+			hasBegun = true
+			TransitionIn()
+		else:
+			return 
+	
 	match state:
 		transStates.IDLE:
 			return 
